9e35a86cd78a720f907a580b5f126bb62a8c0d07
ChristopherKhut <cas012101@coderacademy.edu.au>
2021-12-16 14:52:18 +1100

    ADDED PUTS FOR WHAT USER WANTS MENU

---
+++ main.rb
@@ -0,0 +1,9 @@
+books = {
+
+    A_Clash_Of_Kings: 4,
+    Harry_Potter_and_the_sorcerers_Stone:5,
+    The_lord_of_Rings:7,
+
+}
+
+puts "What would you like to do? Options: (Add) (update) (delete) (list)"
