0fbc354b45f9d54f77c78f55c3c0e8ecf3651610
ChristopherKhut <cas012101@coderacademy.edu.au>
2021-12-16 15:07:47 +1100

    Completed created method

--- main.rb
+++ main.rb
@@ -1,9 +1,7 @@
 books = {
-
     A_Clash_Of_Kings: 4,
     Harry_Potter_and_the_sorcerers_Stone:5,
     The_lord_of_Rings:7,
-
 }
 
 puts "What would you like to do? Options: (add) (update) (delete) (list)"
@@ -16,4 +14,9 @@ when "add"
     puts "What book would you like to add to your list?"
     title = gets.chomp
     puts "What rating would you like to give this book? (Between 1 - 10)"
-    books[title.to_sym]
+    rating = gets.chomp
+    books[title.to_sym] = rating.to_i
+    puts "#{title} has been added to your list with a rating of #{rating}"
+end
+
+puts books
